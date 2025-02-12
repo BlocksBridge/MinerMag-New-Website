458ecaf02db57b09b9e9e75194a295510e742021
shub3am <shubhamvishwakarma0604@gmail.com>
2025-02-12 16:32:21 +0530

    added link to original sec filing

--- app/company/[company]/sec/[cik]/[date]/page.tsx
+++ app/company/[company]/sec/[cik]/[date]/page.tsx
@@ -92,7 +92,7 @@ export default async function SummarisedSEC({
               </div>
               <div className="bg-gray-50 px-6 py-4">
                 <Link
-                  href={`/sec-filings/${filing.id}`}
+                  href={findCorrectFiling[0].finalLink}
                   className="inline-flex items-center text-blue-600 hover:text-blue-800">
                   <FileText className="w-5 h-5 mr-2" />
                   View Original SEC Filing
