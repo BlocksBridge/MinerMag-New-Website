426fd2ebfe45511e339811191f7cea2151be1285
shub3am <shubhamvishwakarma0604@gmail.com>
2025-02-18 12:46:10 +0530

    fixed network data on market page UI

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -20,7 +20,7 @@ export default async function ConsolidatedCompanies() {
       <h1 className="font-bold"> Bitcoin Mining Companies Statistics</h1>
       {/* Market Data Section */}
       <section className="flex justify-center items-centerm m-auto">
-        <div className="grid md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
           {[
             {
               title: "Network Difficulty",
