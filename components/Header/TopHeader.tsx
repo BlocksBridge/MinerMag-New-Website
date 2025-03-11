86e4764ef0e893d98d78da16c61c1f39fa8e6daf
shub3am <shubhamvishwakarma0604@gmail.com>
2025-03-11 20:45:01 +0530

    Added Hyperlink to top Banner

--- components/Header/TopHeader.tsx
+++ components/Header/TopHeader.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 export default async function TopHeader() {
   const getLeaderBoard = await fetch(
     `${process.env.NEXT_PUBLIC_backend_url}/wp-json/wp/v2/leaderboard?acf_format=standard`
@@ -7,18 +8,20 @@ export default async function TopHeader() {
   const checkHeaderAd = await fetch(
     `${process.env.NEXT_PUBLIC_backend_url}/wp-json/ad-banners/v1/header-top-banner`
   ).then((res) => res.json());
-
+  console.log(checkHeaderAd);
   return (
     <div className="w-10/12 md:w-6/12 mx-auto bg-white p-4 rounded-lg ">
       {checkHeaderAd?.html ? (
-        <div>
+        <Link
+          href={checkHeaderAd.html.split('href="')[1].split(`"`)[0]}
+          target="_blank">
           <Image
             alt="Ad Banner"
             src={checkHeaderAd.html.split("src=")[1].split('"')[1]}
             width={728}
             height={90}
           />
-        </div>
+        </Link>
       ) : (
         <Image
           src={getLeaderBoard[0].acf.banner}
