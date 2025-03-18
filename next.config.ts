c9b6e0685c302592bf2adbc47bc59653986e8e90
shub3am <shubhamvishwakarma0604@gmail.com>
2025-03-18 20:50:00 +0700

    enabled hmr cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {  experimental: {
-  serverComponentsHmrCache: false, // defaults to true
+ 
 }, images: {remotePatterns: [{protocol: "https", hostname: "backend.theminermag.com"},{protocol: "https", hostname: "theminermag.com"}],
 }, typescript: {ignoreBuildErrors: true}, eslint: {ignoreDuringBuilds: true}
   /* config options here */
