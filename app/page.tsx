fe8a7a2ed1ea7359777a2998ef2e2ffec0e32beb
shub3am <shubhamvishwakarma0604@gmail.com>
2025-03-20 11:00:00 +0530

    fixed caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import HomePage from "@/components/Home/Home";
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  let par = await searchParams;
   const getPosts: [any] = await fetch(
-    `${process.env.NEXT_PUBLIC_backend_url}/wp-json/wp/v2/posts?acf_format=standard`,
+    `${
+      process.env.NEXT_PUBLIC_backend_url
+    }/wp-json/wp/v2/posts?acf_format=standard&${par ? "" : ""}`,
     {
       cache: "no-cache",
       next: { tags: ["posts"], revalidate: 60 },
     }
   ).then((res) => res.json());
+  console.log("par", par);
   const getBitcoinPrice = await fetch(
     "https://api.hashrateindex.com/v1/hashrateindex/coin/bitcoin/price",
     { headers: { "X-Hi-Api-Key": "hi.72efa27d0f82a2541260cfb351e3adc7" } }
@@ -35,3 +39,5 @@ export default async function Page() {
 }
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
