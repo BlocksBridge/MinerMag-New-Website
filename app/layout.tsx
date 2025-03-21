0340ff3905642dfb286561e7c9f7d974216995ff
shub3am <shubhamvishwakarma0604@gmail.com>
2025-03-21 23:22:58 +0530

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/Logofavicon.ico" />
+        <link rel="apple-touch-icon" href="/Logofavicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <TopHeader />
