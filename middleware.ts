6e6b2ea1cbdfa247e0656e6a980f28133bba4d15
shub3am <shubhamvishwakarma0604@gmail.com>
2025-04-03 14:36:46 +0530

    middleware moddified

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,16 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/api/emails/sendReport") {
+    request.headers.append("Access-Control-Allow-Credentials", "true");
+    request.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+    request.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT"
+    );
+    request.headers.append(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
     return NextResponse.next();
   }
 
