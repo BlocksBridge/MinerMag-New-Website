6a99a5dae8e52e9097137e646349149c2c23f49f
shub3am <shubhamvishwakarma0604@gmail.com>
2025-04-16 23:05:28 +0530

    Heatmap link added to homepage

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -342,6 +342,28 @@ export default async function HomePage({
                   cta: "Read Research",
                   link: "/research",
                 },
+                {
+                  icon: (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-8 w-8 text-blue-600"
+                      fill="none"
+                      viewBox="0 24"
+                      stroke="currentColor">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 00-2-2H7a2 00-2 2v10a2 002 2zM9 9h6v6H9V9z"
+                      />
+                    </svg>
+                  ),
+                  title: "Bitcoin Mining Heatmap",
+                  description:
+                    "Explore Operational Bitcoin Mining Power Capacity Distribution in North America",
+                  cta: "View Heatmap",
+                  link: "/heatmap",
+                },
               ].map((item, index) => (
                 <div key={index} className="bg-white p-4 rounded-lg shadow">
                   {item.icon}
