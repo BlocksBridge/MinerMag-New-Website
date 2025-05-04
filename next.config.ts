b64c65d5de9af54998a3a106f4b1a67b3146963c
shub3am <shubham@blocksbridge.com>
2025-05-04 18:24:40 +0000

    deployment ready

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = {output: 'standalone',
   experimental: { serverComponentsHmrCache: false },
 
   images: {
