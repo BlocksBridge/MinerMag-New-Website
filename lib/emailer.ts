49c72b51c198ce13ac6a685bc94043d7a78e589c
shub3am <shubhamvishwakarma0604@gmail.com>
2025-05-10 01:45:14 +0530

    checked

--- lib/emailer.ts
+++ lib/emailer.ts
@@ -18,6 +18,7 @@ export async function sendEmail(receiverEmail, subject, body) {
       },
       (err, info) => {
         if (err) {
+          console.error(err);
           reject(err);
         } else {
           resolve(info);
