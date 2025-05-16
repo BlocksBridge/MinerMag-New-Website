f3b655df0fc711fac9cc449b6f6c95db3cdbb8df
shub3am <shubhamvishwakarma0604@gmail.com>
2025-05-16 15:21:23 +0530

    Revert "SSG"
    This reverts commit e65eca5a746c20c24fd1fcc2040374c6b9539d8e.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import HomePage from "@/components/Home/Home";
+import { headers } from "next/headers";
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const headersList = await headers();
+
+  let par = await searchParams;
+
   const getPosts: [any] = await fetch(
-    `${process.env.NEXT_PUBLIC_backend_url}/wp-json/wp/v2/posts?acf_format=standard`,
+    `${
+      process.env.NEXT_PUBLIC_backend_url
+    }/wp-json/wp/v2/posts?acf_format=standard&_=${Date.now()}`,
     { next: { revalidate: 3600 } }
   ).then((res) => res.json());
 
